9f95d7d2410102f374ca51af2f8983d7dd86f741
Monsterovich <monsterovich@gmail.com>
2020-05-05 13:09:48 +0200

    Eleminate QML warnings

--- sources/cpp/main.cpp
+++ sources/cpp/main.cpp
@@ -650,6 +650,9 @@ int main(int argc, char *argv[])
 	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
 	QGuiApplication app(argc, argv);
+	// eleminate QML warnings
+	app.setOrganizationName("protox");
+	app.setOrganizationDomain("org");
 
 	qInstallMessageHandler(customMessageHandler);
 
